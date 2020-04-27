bc5055f5df4c29036c6995a1b79dd7f173a96a33
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-27 17:47:01 +0530

    Add html code for fetch.

--- ajax.js
+++ ajax.js
@@ -3,4 +3,17 @@ fetch(`https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=10`)
   .then((data) => {
     const facts = data.map((catData) => catData.text);
     console.log(facts);
+    fillData(facts);
   });
+
+function fillData(arr) {
+  const list = document.querySelector('#list');
+
+  let htmlString = arr.map((fact) => '<li>' + fact + '</li>');
+  htmlString = htmlString.join('');
+  console.log(htmlString);
+  list.innerHTML = htmlString;
+}
+
+const arr = ['a', 'd', 'a', 'r', 's', 'h'];
+console.log(arr.join(''));
